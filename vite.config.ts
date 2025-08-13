f924e53f77ec5c6aa130e712c07b3f11bf2f3573
Kiran Babu <Kiran.Babu3@in.ey.com>
2025-08-13 14:05:36 +0530

    Set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/hello-world-actions/', // 👈 set to your repo name
+});
